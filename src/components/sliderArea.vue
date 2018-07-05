74c85eb55ac6d23a79163c487e4cdc6faedb779a
lx <122698565@qq.com>
2018-07-05 18:42:57 +0800

    18-7.5

--- src/components/sliderArea.vue
+++ src/components/sliderArea.vue
@@ -3,8 +3,8 @@
         11
         <hr>
         <div class="item">
-            <span>Spell</span>
-            <div>
+            <span class="words">Spell</span>
+            <div class="Yinbiao">
                 <span>
                     <img src="../images/Yinbiao.png" alt="小喇叭">
                     <span>[SPEL]</span>
@@ -16,12 +16,12 @@
                     <span>英</span>
                 </span>
             </div>
-            <div>遍历</div>
-            <div>
+            <div class="Zhiyi">遍历</div>
+            <div class="translate">
                 <li>
                     <span>vt:</span>
-                    <span>xxxx</span>
-                    <span>xxxx</span>
+                    <span>行动、翻译、动作</span>
+                    <span>测试大小、文字测试</span>
                 </li>
                 <li>
                     <span>vi:</span>
@@ -48,7 +48,7 @@
               {content:'src/images/2.png'},
               {content:'src/images/3.png'}
             ]
-          new islider(document.querySelector('.isliderBox'),imgList)
+          // new islider(document.querySelector('.isliderBox'),imgList)
         }
   };
 </script>
@@ -58,5 +58,28 @@
     .sliderArea{
         margin-top: 0.4rem;
         background-color: rgba(220,20,60,0.1);
+        .item{
+            margin: 0 auto;
+            width: 3.5rem;
+            height: 3.5rem;
+            border: 0.01rem solid @standard_color;
+            .words{
+                font-family: Aharoni;
+                font-size: 0.45rem;
+           }
+            .Yinbiao{
+                font-size: 0.12rem;
+                img{
+                    width: 0.2rem;
+                    height: 0.2rem;
+                }
+            }
+            .Zhiyi{
+                font-size: 0.3rem;
+            }
+            .translate{
+                font-size: 0.14rem;
+            }
+        }
     }
 </style>
